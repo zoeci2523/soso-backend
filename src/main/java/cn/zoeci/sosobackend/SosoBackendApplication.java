69e5745d575dcb362fd409e077e5bcfb31f26eca
zoeci2523 <13192677392@163.com>
2023-10-03 10:21:14 +0800

    Initial commit

---
+++ src/main/java/cn/zoeci/sosobackend/SosoBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.zoeci.sosobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosoBackendApplication.class, args);
+    }
+
+}
